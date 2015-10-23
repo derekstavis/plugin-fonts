6518fd5664a07cce9dec1236ca604abadf56d610
Derek Stavis <derek@dba.eng.br>
2015-10-23 16:19:47 -0200

    Support OS X user font path

--- init.fish
+++ init.fish
@@ -2,14 +2,17 @@ function init -a path --on-event init_fonts
   set -g FONT_MOCK_INDEX "$path/mock_index.json"
   set -g FONT_MOCK_SHOW "$path/mock_show.json"
 
-  set -q XDG_DATA_HOME;
-      or set -l XDG_DATA_HOME "$HOME/.local"
+  if not set -q FONTS_PATH
+    if test 'Darwin' = (uname)
+      set -g FONTS_PATH "$HOME/Library/Fonts"
+    else
+      set -q XDG_DATA_HOME; or set -l XDG_DATA_HOME "$HOME/.local"
+      set -g FONTS_PATH "$XDG_DATA_HOME/share/fonts"
+    end
+  end
 
-  set -q FONTS_PATH;
-    or set -g FONTS_PATH "$XDG_DATA_HOME/share/fonts"
 
   set -g FONTS_CONFIG "$OMF_CONFIG/fonts"
-
   mkdir -p $FONTS_CONFIG
 
 end
