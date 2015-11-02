cb44914678553a154ac25a6e466e52fa6da3296c
Derek Stavis <dekestavis@gmail.com>
2015-11-02 15:24:49 -0200

    repo: add apache font directory

--- functions/__fonts.repo.list.fish
+++ functions/__fonts.repo.list.fish
@@ -1,4 +1,4 @@
 function __fonts.repo.list
-  set -l url 'https://api.github.com/repos/google/fonts/contents/ofl'
+  set -l url 'https://api.github.com/repos/google/fonts/contents'/{ofl,apache}
   wget -q -O- $url | grep '"name"' | sed 's/[",]//g;s/name://g;s/^ *//;s/ *$//'
 end
