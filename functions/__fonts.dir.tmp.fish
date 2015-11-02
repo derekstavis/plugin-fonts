29855268660e81221db4abee7773e1abd1ebb353
Derek Stavis <dekestavis@gmail.com>
2015-11-02 15:25:09 -0200

    remove unused file

--- functions/__fonts.dir.tmp.fish
+++
@@ -1,9 +0,0 @@
-function __fonts.dir.tmp -a name
-  test -z name; and return 1
-
-  if set -l tmpdir (mktemp -q /tmp/$name.XXXXXX)
-
-  else
-    echo "Can't create a temporary directory!"
-  end
-end
