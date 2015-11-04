5b0129c40f5a4273ac3fba68a886ed93780b6e78
Derek Stavis <dekestavis@gmail.com>
2015-11-04 00:51:10 -0200

    completions: fix code style (column alignment)

--- completions/fonts.fish
+++ completions/fonts.fish
@@ -10,8 +10,8 @@ end
 
 complete -c fonts -f -d "Simple font manager"
 
-complete -c fonts -f -n "__fish_seen_subcommand_from install"            -a "(__fonts_complete_install)"
-complete -c fonts -f -n "__fish_seen_subcommand_from remove"            -a "(__fonts_complete_remove)"
+complete -c fonts -f -n "__fish_seen_subcommand_from install" -a "(__fonts_complete_install)"
+complete -c fonts -f -n "__fish_seen_subcommand_from remove"  -a "(__fonts_complete_remove)"
 
 complete -c fonts -f -n "__fish_use_subcommand" -a help    -d "Show help message"
 complete -c fonts -f -n "__fish_use_subcommand" -a install -d "Download and install <font_name>"
