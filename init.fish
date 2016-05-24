930d566fc2b6eb5849afb4695e695405f288afb3
Derek Stavis <dekestavis@gmail.com>
2016-05-23 22:55:18 -0300

    init: simplify. bye events!

--- init.fish
+++ init.fish
@@ -1,19 +1,18 @@
-function init -a path --on-event init_fonts
+set path $argv
+autoload $argv/functions/{config,repo}
 
-  autoload $path/functions/{config,repo}
+set -g __fonts_providers 'google' 'powerline'
 
-  set -g __fonts_providers 'google' 'powerline'
-
-  if not set -q FONTS_PATH
-    if test 'Darwin' = (uname)
-      set -g FONTS_PATH "$HOME/Library/Fonts"
-    else
-      set -q XDG_DATA_HOME; or set -l XDG_DATA_HOME "$HOME/.local"
-      set -g FONTS_PATH "$XDG_DATA_HOME/share/fonts"
-    end
+if not set -q FONTS_PATH
+  if test 'Darwin' = (uname)
+    set -g FONTS_PATH "$HOME/Library/Fonts"
+  else
+    set -q XDG_DATA_HOME; or set -l XDG_DATA_HOME "$HOME/.local"
+    set -g FONTS_PATH "$XDG_DATA_HOME/share/fonts"
   end
-
-  set -g FONTS_CONFIG "$OMF_CONFIG/fonts"
-  mkdir -p $FONTS_CONFIG
-
 end
+
+set -g FONTS_CONFIG "$OMF_CONFIG/fonts"
+
+test -e $FONTS_CONFIG
+  or mkdir -p $FONTS_CONFIG
